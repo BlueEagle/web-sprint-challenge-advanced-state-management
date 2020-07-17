bf267312934804f30ae9520045b72377d6ac51ad
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-07-17 11:26:43 -0600

    wip

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -30,9 +30,6 @@ class App extends Component {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
 
         <h2>Add Smurfs</h2>
         <label>Name: <input type="text" name="name" value={this.state.name} onChange={this.handleChanges}></input></label>
